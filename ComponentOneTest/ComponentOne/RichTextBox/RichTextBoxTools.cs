fed419ecc7560dc6370a6853c472180b5b2b0c0a
Keisuke Hatanaka <pontacoss@outlook.jp>
2024-01-21 22:28:29 +0900

    2024/1/21

---
+++ ComponentOneTest/ComponentOne/RichTextBox/RichTextBoxTools.cs
@@ -0,0 +1,309 @@
+﻿using C1.WPF.RichTextBox.Documents;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace ComponentOneTest.ComponentOne.RichTextBox
+{
+
+    public static class RichTextBoxTools
+    {
+        private static C1TableCell CreateCell(string? name)
+        {
+            var cell = new C1TableCell();
+            cell.BorderThickness = new Thickness(1);
+            var paragraph = new C1Paragraph();
+            paragraph.Children.Add(
+                new C1Run()
+                {
+                    Text = name,
+                    Padding = new Thickness(5, 0, 5, 0)
+                });
+            paragraph.Padding = new Thickness(0);
+            cell.Children.Add(paragraph);
+            cell.Padding = new Thickness(0);
+            return cell;
+        }
+
+        public static C1Table CreateTable<T>(IList<T> list) where T : class
+        {
+            var table = new C1Table();
+            var properties = typeof(T).GetProperties();
+
+            //var columnNamesProperty = typeof(T).GetProperty("ColumnNames");
+            //var columnNames =  columnNamesProperty?.GetValue(list[0]) as List<string>;
+
+            // カラムの作成
+            for (int i = 0; i < properties.Count(); i++)
+            {
+                table.Columns.Add(new C1TableColumn());
+            }
+            //if (columnNames == null) return null;
+            //foreach (var col in columnNames)
+            //{
+            //    table.Columns.Add(new C1TableColumn());
+            //}
+
+            // タイトル行の生成
+            var titleRow = new C1TableRow();
+            foreach (var property in properties)
+            {
+                var cell = CreateCell(property.Name);
+                cell.TextAlignment = C1TextAlignment.Center;
+                cell.Background = new SolidColorBrush(Colors.LightGray);
+                titleRow.Children.Add(cell);
+            }
+            table.Children.Add(titleRow);
+
+            //foreach(var col in columnNames)
+            //{
+            //    var cell = CreateCell(col);
+            //    cell.TextAlignment = C1TextAlignment.Center;
+            //    cell.Background = new SolidColorBrush(Colors.LightGray);
+            //    titleRow.Children.Add(cell);
+            //}
+            //table.Children.Add(titleRow);
+
+            // データ行の生成
+            foreach (var entity in list)
+            {
+                var valueRow = new C1TableRow();
+                foreach (var prop in properties)
+                {
+                    var valueObj = prop.GetValue(entity);
+                    var value = valueObj?.ToString();
+                    valueRow.Children.Add(CreateCell(value));
+                }
+                table.Children.Add(valueRow);
+            }
+
+            // テーブル設定
+            table.BorderCollapse = true;
+            table.TableAlignment = C1TextAlignment.Center;
+            table.Padding = new Thickness(0);
+
+            return table;
+        }
+
+        public static C1Table CreateTable(TableEntity table)
+        {
+
+
+            int rowHeaderDepth = GetHeaderDepth(table.RowHeaders);
+            int rowHeaderWidth = GetHeaderWidth(table.RowHeaders);
+
+            int columnHeaderDepth = GetHeaderDepth(table.ColumnHeaders);
+            int columnHeaderWidth = GetHeaderWidth(table.ColumnHeaders);
+
+
+            var c1Table = new C1Table();
+            // カラムの作成
+            for (int i = 0; i < rowHeaderDepth+columnHeaderWidth; i++)
+            {
+                c1Table.Columns.Add(new C1TableColumn());
+            }
+            // Rowの作成
+            for (var i = 0; i < columnHeaderDepth; i++)
+            {
+                var titleRow = new C1TableRow();
+
+                if (i == 0)
+                {
+                    for (int j = 0; j < rowHeaderDepth; j++)
+                    {
+                        var cell = CreateCell("111");
+                        cell.TextAlignment = C1TextAlignment.Center;
+                        cell.Background = new SolidColorBrush(Colors.LightGray);
+                        cell.RowSpan = columnHeaderDepth;
+                        titleRow.Children.Add(cell);
+
+                    }
+                }
+
+                var list = new List<HeaderContainer>();
+
+                
+                  list.AddRange(GetLevelList(table.ColumnHeaders, i));
+                
+
+
+
+
+
+                foreach(var col in list)
+                {
+                    var cell = CreateCell(col.Name);
+                    cell.TextAlignment = C1TextAlignment.Center;
+                    cell.Background = new SolidColorBrush(Colors.LightGray);
+                    cell.ColumnSpan = GetEndNode(col);
+                    titleRow.Children.Add(cell);
+                }
+                c1Table.Children.Add(titleRow);
+
+
+            }
+
+            c1Table.BorderCollapse = true;
+
+            return c1Table;
+        }
+
+        private static IEnumerable<HeaderContainer> GetLevelList(IEnumerable<HeaderContainer> columns, int level)
+        {
+            var list=new List<HeaderContainer>();
+
+            foreach(var col in columns)
+            {
+                if (col.Level == level)
+                {
+                    list.Add(col);
+                }
+                else
+                {
+                    list.AddRange(GetLevelList(col.Children, level));
+                }
+                
+            }
+
+            return list;
+        }
+
+        private static int GetHeaderWidth(IEnumerable<HeaderContainer> containers)
+        {
+            int width = 1;
+            foreach (var container in containers)
+            {
+                width *= GetEndNode(container);
+            }
+
+            return width;
+        }
+
+        private static int GetEndNode(HeaderContainer container)
+        {
+            int nodeNumber = 0;
+            
+            if (container.Children.Count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                foreach (var child in container.Children)
+                {
+                    nodeNumber += GetEndNode(child);
+                }
+            }
+            
+            return nodeNumber;
+        }
+
+            private static int GetHeaderDepth(IEnumerable<HeaderContainer> containers)
+        {
+            int depth = 0;
+            foreach (var container in containers)
+            {
+                depth += GetMaxLevel(container.Children) + 1;
+            }
+
+            return depth;
+        }
+
+        private static int GetMaxLevel(IList<HeaderContainer> list)
+        {
+            int maxLevel = 0;
+
+            foreach (var entity in list)
+            {
+                if (entity.Children.Count > 0)
+                {
+                    var i = GetMaxLevel(entity.Children);
+                    maxLevel = Math.Max(maxLevel, i);
+                }
+                else
+                {
+                    maxLevel = Math.Max(maxLevel, entity.Level);
+                }
+            }
+            return maxLevel;
+        }
+    }
+
+    public sealed class TableEntity
+    {
+        public string TableName { get; }
+        public IEnumerable<HeaderContainer> RowHeaders { get; }
+        public IEnumerable<HeaderContainer> ColumnHeaders { get; }
+
+        public TableEntity(string tableName,
+            IList<HeaderContainer> rowHeaders,
+            IList<HeaderContainer> columnHeaders)
+        {
+            TableName = tableName;
+            RowHeaders = rowHeaders;
+            ColumnHeaders = columnHeaders;
+        }
+    }
+
+    //public interface ITableHeader
+    //{
+    //    int Id { get; }
+    //    int Parent { get; }
+    //    string Name { get;  }
+    //    int Level { get;  }
+
+    //    void Add(ITableHeader tableHeader);
+    //    IEnumerable<ITableHeader> GetChildren();
+    //}
+
+    public sealed class HeaderContainer 
+    {
+        public List<HeaderContainer> Children { get; } = new List<HeaderContainer>();
+        private HeaderEntity _headerEntity;
+        
+        public string Name => _headerEntity.Name;
+        public int Level =>_headerEntity.Level;
+        public int Id => _headerEntity.Id;
+        public int Parent => _headerEntity.Parent;
+
+        public HeaderContainer(HeaderEntity headerEntity)
+        {
+            _headerEntity = headerEntity;
+        }
+
+        public void Add(HeaderContainer tableHeader)
+        {
+            Children.Add(tableHeader);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+
+
+    public sealed class HeaderEntity
+    {
+        public int Id { get; }
+        public int Parent { get; }
+        public string Name { get; } 
+        public int Level { get;  }
+
+        public HeaderEntity(HeaderEntity? parent, int id, string name)
+        {
+            Id = id;
+
+            Name = name;
+
+            Parent = parent != null ? parent.Id : 0;
+            Level = parent != null ? parent.Level+1 : 0;
+
+        }
+    }
+
+
+
+}
+
--- ComponentOneTest/Window2.xaml.cs
+++ ComponentOneTest/Window2.xaml.cs
@@ -1,13 +1,34 @@
 ﻿using C1.WPF.RichTextBox;
+using C1.WPF.RichTextBox.Documents;
+using ComponentOneTest.ComponentOne.RichTextBox;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace ComponentOneTest
 {
+    public sealed class TreeViewData
+    {
+        public string Name { get; }
+        public List<TreeViewData> Children { get; } = new();
+
+        public TreeViewData(HeaderContainer header)
+        {
+            Name = header.Name;
+
+
+            //foreach (var child in header.GetChildren())
+            //{
+            //    Children.Add(new TreeViewData(child));
+            //}
+        }
+    }
+
     /// <summary>
     /// Window2.xaml の相互作用ロジック
     /// </summary>
     public partial class Window2 : Window
     {
+        private List<HeaderContainer> _headers = new();
         public Window2()
         {
             InitializeComponent();
@@ -18,7 +39,7 @@ namespace ComponentOneTest
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var list = new List<TemplateEntity>();
-            list.Add(new TemplateEntity("Visual inspection","6.1","x","x","4.5.3.1"));
+            list.Add(new TemplateEntity("Visual inspection", "6.1", "x", "x", "4.5.3.1"));
             list.Add(new TemplateEntity("Verification of dimensions and tolerance", "6.2", "x", "-", "4.5.3.2"));
             list.Add(new TemplateEntity("Weighting", "6.3", "x", "-", "4.5.3.3"));
             list.Add(new TemplateEntity("Marking inspection", "6.4", "x", "x", "4.5.3.4"));
@@ -55,8 +76,54 @@ namespace ComponentOneTest
             list.Add(new TemplateEntity("Leakage test", "6.5", "x", "-", "4.5.3.5.4"));
             list.Add(new TemplateEntity("Test of the degree of protection", "6.6", "x", "-", "4.5.3.6"));
 
-            rtb.Document.Blocks.Add(CreateTable.ListType(list));
+            //rtb.Document.Blocks.Add(RichTextBoxTools.CreateTable(list));
+
+            //var table = rtb.Document.Children[1] as C1Table;
+            //var row = table.Children[0] as C1TableRow;
+            //var cell = row.Children[0] as C1TableCell;
+            //cell.RowSpan = 2;
+
+            var data1 = GetItemSource(HeaderFake.GetData());
+            var data2 = GetItemSource(HeaderFake.GetData2());
+
+            tv1.ItemsSource = data1;
+            tv2.ItemsSource = data2;
+
+            var table = new TableEntity("name", data1, data2);
+
+            rtb.Document.Blocks.Add(RichTextBoxTools.CreateTable(table));
         }
 
+        private IList<HeaderContainer> GetItemSource(List<HeaderEntity> list)
+        {
+            var source=new List<HeaderContainer>();
+
+            foreach (var entity in list)
+            {
+                var parent = GetParent(source, entity.Parent);
+                if (parent != null)
+                {
+                    parent.Add(new HeaderContainer(entity));
+                }
+                else
+                {
+                    source.Add(new HeaderContainer(entity));
+                }
+            }
+            return source;
+        }
+
+        private HeaderContainer? GetParent(IList<HeaderContainer> list, int parentId)
+        {
+            foreach (var entity in list)
+            {
+                if (entity.Id == parentId) return entity;
+
+                var target= GetParent(entity.Children, parentId);
+
+                if (target != null) return target;
+            }
+            return null;
+        }
     }
 }
